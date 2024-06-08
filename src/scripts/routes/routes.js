b948559fa0fc5791fa070069cbca9232486f21ae
remind26 <98072400+remind26@users.noreply.github.com>
2024-06-09 06:30:57 +0700

    Update routes.js

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,15 +2,12 @@
 import Home from '../views/pages/home';
 import WeatherWitch from '../views/pages/weather';
 import Aboutus from '../views/pages/aboutus';
-import Musik from '../views/pages/mendengarkan-musik';
 
 const routes = {
   '/': Home,
   '/home': Home,
   '/weather': WeatherWitch,
   '/aboutus': Aboutus,
-  '/mendengarkan-musik': Musik,
-
 };
 
 export default routes;
